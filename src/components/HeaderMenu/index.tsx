8d3e559c199794ad234a506363944c5a3e35b7da
Renildo15 <renildorabi@ufrn.edu.br>
2025-02-12 14:28:11 -0300

    refactor: adicionando cursor point no menu

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -8,20 +8,18 @@ export default function HeaderMenu() {
     return (
         <>
             <div className="flex flex-row gap-4 items-center">
-                 <div className="flex flex-row items-center gap-2">
+                 <div className="flex flex-row items-center gap-2 cursor-pointer">
                      <Image src={Menu} alt="menu" width={19} height={19} />
                      <span className="font-semibold text-[#3F3F3F] text-[14px]">Departamentos</span>
                      <Image src={ChevronDown} alt="chevron-down" width={19} height={19} />
                  </div>
                  <div className="w-[1px] h-[20px] bg-[#3F3F3F] opacity-30"></div>
-                 <div className="flex flex-row items-center gap-2">
+                 <div className="flex flex-row items-center gap-2 cursor-pointer">
                      <Image src={EIcon} alt="menu" width={19} height={19} />
                      <span className="text-[#3F3F3F] text-[14px]">Descontos Cliente Unimed Natal</span>
                  </div>
-             </div>
-             <div>
-                 <span className="text-[#737373] text-[14px]">Venda no Uniclube</span>
-             </div>
+            </div>
+            <span className="text-[#737373] text-[14px] cursor-pointer">Venda no Uniclube</span>
         </>
     )
 }
