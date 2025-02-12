dfa1842a3245d4cdbecb0df7f32b824d7bae31b0
Renildo15 <renildorabi@ufrn.edu.br>
2025-02-12 14:27:47 -0300

    refactor: renomeando nome de componente

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,9 +1,9 @@
 import { categories } from "@/data";
 import CategoryItem from "../CategoryItem";
 
-export default function CategoryList() {
+export default function Categories() {
     return (
-        <div className="container mx-auto px-10">
+        <div className="container mx-auto px-14">
             <div className="flex flex-wrap justify-between gap-x-1 gap-y-4">
                 {categories.map((category, index) => (
                     <CategoryItem key={index} category={category}/>
