3126aab0046b95dd5ad1cf52ee674f59dcc1e31b
Renildo15 <renildorabi@ufrn.edu.br>
2025-02-12 10:07:54 -0300

    feat: usando componente de categoria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import CategoryList from "@/components/CategoryList";
 
 export default function Home() {
   return (
     <main className=" flex flex-col gap-6 h-full bg-white">
       <Header/>
       <Banner/>
+      <CategoryList/>
     </main>
   );
 }
